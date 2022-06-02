47b2f814d881a7aeb66657b63a97b57953754992
hicham <erragragui.hicham@gmail.com>
2022-06-02 16:42:24 +0100

    nginx customized webpage

--- locals.tf
+++ locals.tf
@@ -5,5 +5,5 @@ locals {
   aws_ecr_url = aws_ecr_repository.repository.repository_url
   tag = random_uuid.tag.result
   dockerinstanceip = aws_instance.dockerinstance.public_ip
-  private_key = "${path.root}/dockerinstancesshkey.pem"
+  dockerinstanceprivate_key = "${path.root}/dockerinstancesshkey.pem"
 }
