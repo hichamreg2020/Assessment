58ed187023408bf8649bf867d7e2d7b40e90b424
hicham <erragragui.hicham@gmail.com>
2022-06-02 12:58:13 +0100

    add loadbalancer external ip output

--- webserver.tf
+++ webserver.tf
@@ -71,5 +71,5 @@ resource "kubernetes_secret" "assessment-db-credentials" {
 }
 
 output "load_balancer_external_ip" {
-  value = "${kubernetes_service.web.load_balancer_ingress.0.ip}"
+  value = "${kubernetes_service.web.ingress}"
 }
