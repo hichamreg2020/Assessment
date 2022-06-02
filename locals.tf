a169e3036d752d8ea3aa50edfa7d55bcbd47f10d
hicham <erragragui.hicham@gmail.com>
2022-06-03 00:10:40 +0100

    docker provider

--- locals.tf
+++
@@ -1,9 +0,0 @@
-resource "random_uuid" "tag" {
-}
-
-locals {
-  aws_ecr_url = aws_ecr_repository.repository.repository_url
-  tag = random_uuid.tag.result
-  dockerinstanceip = aws_instance.dockerinstance.public_ip
-  dockerinstanceprivate_key = "${path.root}/dockerinstancesshkey.pem"
-}
