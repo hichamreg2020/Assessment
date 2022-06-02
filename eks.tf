c203fba56746d1faeeb36fe6e89a19bad116f10a
hicham <erragragui.hicham@gmail.com>
2022-06-02 09:09:59 +0100

    eks fix

--- eks.tf
+++ eks.tf
@@ -3,7 +3,7 @@
  
   source          = "terraform-aws-modules/eks/aws"
   version         = "17.24.0"
-  cluster_name    = "kubeAssess"
+  cluster_name    = "kubeAssessment"
   cluster_version = "1.21"
   subnets         = module.vpc_assess.private_subnets
   vpc_id = module.vpc_assess.vpc_id
