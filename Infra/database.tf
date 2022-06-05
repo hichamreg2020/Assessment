69fbfc3f72f1145e1bfb64ed4587b73cec48be08
hichamreg2020 <65364422+hichamreg2020@users.noreply.github.com>
2022-06-05 12:40:08 +0100

    Update database.tf

--- Infra/database.tf
+++ Infra/database.tf
@@ -59,7 +59,7 @@ resource "aws_db_instance" "assessment-db" {
 
 
 resource "aws_secretsmanager_secret" "Assessment-db_secret" {
-  name = "Assessment-db-credentials"
+  name = "Assessment-dbcredentials"
 }
 
 resource "aws_secretsmanager_secret_version" "secret" {
